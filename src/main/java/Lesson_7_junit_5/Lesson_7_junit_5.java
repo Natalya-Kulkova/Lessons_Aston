a620cc7e041d8a63844ae61662aa671750aa0748
NatalyaKulkova <adissinia@gmail.com>
2024-10-20 15:24:33 +0300

    ДЗ Д14

---
+++ src/main/java/Lesson_7_junit_5/Lesson_7_junit_5.java
@@ -0,0 +1,5 @@
+package Lesson_7_junit_5;
+
+public class Lesson_7_junit_5 {
+
+}
---
+++ src/test/java/Lesson_7_junit_5/Factorial.java
@@ -0,0 +1,14 @@
+package Lesson_7_junit_5;
+
+public class Factorial {
+    public static long calculateFactorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Число не должно быть отрицательным");
+        }
+        long result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
